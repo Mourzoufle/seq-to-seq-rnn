aa083e839a15e67f28f0f1cb7ddeba99497ac048
Mourzoufle <Mourzoufle@163.com>
2016-06-28 22:08:23 +0800

    first stable ver.

--- seq_to_seq_rnn.py
+++ seq_to_seq_rnn.py
@@ -1,4 +1,4 @@
-﻿from __future__ import print_function
+from __future__ import print_function
 import time
 import json
 import numpy
@@ -12,16 +12,16 @@ from theano.sandbox.rng_mrg import MRG_RandomStreams
 SEED = 65535
 
 
-def get_batches(n_sample, batch_size, shuffle=False):
+def get_batches(n_samples, batch_size, shuffle=False):
 	"""
 	Get batches of data
 	"""
-	indices = numpy.arange(n_sample, dtype="int32")
+	indices = numpy.arange(n_samples, dtype="int32")
 	if shuffle:
 		numpy.random.shuffle(indices)
 	batches = []
 	idx_start = 0
-	while idx_start < n_sample:
+	while idx_start < n_samples:
 		batches.append(indices[idx_start: idx_start + batch_size])
 		idx_start += batch_size
 
@@ -328,15 +328,13 @@ def pred_error(f_prob, samples, batches, mat):
 	"""
 	Compute the prediction error
 	"""
-	errs = []
+	err = 0.
 	for batch in batches:
 		x, mask_x, y, mask_y = load_batch(samples, batch, mat)
 		preds = f_prob(x, mask_x, y, mask_y).argmax(axis=-1)
-		mat_err = (preds != y) * mask_y
-		for i in range(mat_err.shape[0]):
-			errs.apppend(mat_err[i].sum() / mask_y[i].sum())
+		err += (((preds != y) * mask_y).sum(axis=0).astype(config.floatX) / mask_y.sum(axis=0)).sum()
 
-	return preds, numpy.mean(errs)
+	return preds, err / len(samples)
 
 
 def main_model(
@@ -347,32 +345,44 @@ def main_model(
 	path_mat_train='VGG19_train.npy',	# Path of image features of training set
 	path_mat_val='VGG19_val.npy',		# Path of image features of validation set
 	path_mat_test='VGG19_test.npy',		# Path of image features of testing set
+	max_samples_train=0,				# Max number of samples in training
+	max_samples_val=0,					# Max number of samples in validating
+	max_samples_test=0,					# Max number of samples in testing
+	# Model configuration
+	n_dim_img=4096,						# Image feature dimension
+	n_dim_txt=0,						# Vocabulary size
+	n_dim_enc=256,						# Number of hidden units in encoder
+	n_dim_dec=256,						# Number of hidden units in decoder
+	batch_size_train=64,				# Batch size in training
+	batch_size_pred=256,				# Batch size in validation / testing
+	optimizer=rmsprop,					# [sgd|adadelta|rmsprop], sgd not recommanded
+	lrate=0.0001,						# Learning rate for optimizer
+	max_epochs=1000,					# Maximum number of epoch to run
+	patience=10,						# Number of epoch to wait before early stop if no progress
+	# Save & Load
+	path_load=None,						# Path to load a previouly trained model
+	path_save='model.npy',				# Path to save the best model
 	path_out_train='out_train.npy',		# Path to save predicted sentences of training set
 	path_out_val='out_val.npy',			# Path to save predicted sentences of validation set
 	path_out_test='out_test.npy',		# Path to save predicted sentences of testing set
-	# Model configuration
-	n_dim_img=4096,						# Image feature dimension
-	n_dim_txt=-1,						# Vocabulary size
-	n_dim_enc=256,						# Number of hidden units in encoder
-	n_dim_dec=256,						# Number of hidden units in decoder
-	batch_size_train=64,				# batch size in training
-	batch_size_val=256,					# batch size in validation
-	batch_size_test=256,				# batch size in testing
-	optimizer=adadelta,					# [sgd|adadelta|rmsprop], sgd not recommanded
-	lrate=0.0001,						# Learning rate for optimizer
-	max_epochs=5000,					# Maximum number of epoch to run
-	patience=10,						# Number of epoch to wait before early stop if no progress
 	# MISC
-	freq_disp=10,						# Display the training progress after this number of updates
-	freq_valid=370,						# Compute the validation error after this number of updates
-	freq_save=1110,						# Save the parameters after this number of updates
-	path_load=None,						# Path to load a previouly trained model
-	path_save='model.npz',				# Path to save the best model
+	freq_disp=20,						# Display the training progress after this number of updates
+	freq_val=200,						# Compute the validation error after this number of updates
+	freq_save=1000,						# Save the parameters after this number of updates
 ):
 	print('Loading data...')
 	samples_train, mat_train, n_dim_txt = load_data(path_train, path_mat_train, n_dim_txt)
 	samples_val, mat_val, n_dim_txt = load_data(path_val, path_mat_val, n_dim_txt)
 	samples_test, mat_test, n_dim_txt = load_data(path_test, path_mat_test, n_dim_txt)
+	if max_samples_train > 0:
+		samples_train = samples_train[: max_samples_train]
+	if max_samples_val > 0:
+		samples_val = samples_val[: max_samples_val]
+	if max_samples_test > 0:
+		samples_test = samples_test[: max_samples_test]
+	print('\ttraining:   %6d samples' % len(samples_train))
+	print('\tvalidation: %6d samples' % len(samples_val))
+	print('\ttesting:    %6d samples' % len(samples_test))
 
 	print('Building model...')
 	numpy.random.seed(SEED)
@@ -406,93 +416,88 @@ def main_model(
 	f_prob = theano.function([x, mask_x, y, mask_y], output, name='f_prob')
 	f_grad_shared, f_update = optimizer(tensor.scalar(name='lr'), t_params, grads, x, mask_x, y, mask_y, cost)
 
-	print('Optimization...')
-	print('%d training samples' % len(samples_train))
-	print('%d validation samples' % len(samples_val))
-	print('%d testing samples' % len(samples_test))
-	batches_valid = get_batches(len(samples_val), batch_size_val)
-	batches_test = get_batches(len(samples_test), batch_size_test)
+	print('Training...')
+	batches_train = get_batches(len(samples_train), batch_size_train, shuffle=True)
+	batches_val = get_batches(len(samples_val), batch_size_pred)
+	batches_test = get_batches(len(samples_test), batch_size_pred)
 	history_errs = []
 	best_p = None
 	bad_count = 0
-	if freq_valid <= 0:
-		freq_valid = len(samples_train) / batch_size_train
+	if freq_val <= 0:
+		freq_val = len(batches_train)
 	if freq_save <= 0:
-		freq_save = len(samples_train) / batch_size_train
-	n_updates = 0	# the number of update done
-	stop = False	# early stop
+		freq_save = len(batches_train) * max_epochs + 1	# No automatic saving
+	n_batches = 0
+	stop = False
 	time_start = time.time()
 
-	for epoch in range(max_epochs):
-		n_samples = 0
-		# Get new shuffled index for the training set.
-		batches_train = get_batches(len(samples_train), batch_size_train, shuffle=True)
+	for epoch in range(1, max_epochs + 1):
 		for batch in batches_train:
-			n_updates += 1
+			n_batches += 1
 			use_dropout.set_value(1.)
 			x, mask_x, y, mask_y = load_batch(samples_train, batch, mat_train)
-			n_samples += x.shape[1]
 			cost = f_grad_shared(x, mask_x, y, mask_y)
 			f_update(lrate)
 			
 			if numpy.isnan(cost) or numpy.isinf(cost):
-				print('Bad cost detected: ', cost)
+				print('ERROR: bad cost detected!')
 				return
 
-			if numpy.mod(n_updates, freq_disp) == 0:
-				print('Epoch: ', epoch, 'Update: ', n_updates, 'Cost: ', cost)
+			if numpy.mod(n_batches, freq_disp) == 0:
+				print('\tEPOCH: %6d    BATCH: %6d    COST: %12.6f' % (epoch, n_batches, cost))
 
-			if path_save and numpy.mod(n_updates, freq_save) == 0:
-				print('Saving...')
+			if path_save and numpy.mod(n_batches, freq_save) == 0:
+				print('Saving model...')
 				if best_p is not None:
 					params = best_p
 				else:
 					params = unzip(t_params)
 				numpy.save(path_save, params)
-				print('Done.')
 
-			if numpy.mod(n_updates, freq_valid) == 0:
+			if numpy.mod(n_batches, freq_val) == 0:
+				print('Validating...')
 				use_dropout.set_value(0.)
-				err_train = pred_error(f_prob, samples_train, batches_train, mat_train)
-				err_val = pred_error(f_prob, samples_val, batches_val, mat_val)
-				err_test = pred_error(f_prob, samples_test, batches_test, mat_test)
+				_, err_train = pred_error(f_prob, samples_train, get_batches(len(samples_train), batch_size_pred), mat_train)
+				_, err_val = pred_error(f_prob, samples_val, batches_val, mat_val)
+				_, err_test = pred_error(f_prob, samples_test, batches_test, mat_test)
 				history_errs.append([err_val, err_test])
 
 				if (best_p is None or err_val <= numpy.array(history_errs)[:, 0].min()):
 					best_p = unzip(t_params)
-					bad_counter = 0
-				print('Training: ', err_train, 'Validation: ', err_val, 'Testing: ', err_test)
-				if (len(history_errs) > patience and err_val >= numpy.array(history_errs)[:-patience, 0].min()):
-					bad_counter += 1
-					if bad_counter > patience:
-						print('Early stop!')
+					bad_count = 0
+				print('ERR_TRA: %.6f    ERR_VAL: %.6f    ERR_TES: %.6f' % (err_train, err_val, err_test))
+				if len(history_errs) > patience and err_val >= numpy.array(history_errs)[: -patience, 0].min():
+					bad_count += 1
+					if bad_count > patience:
+						print('WARNING: early stop!')
 						stop = True
 						break
 		if stop:
 			break
-
 	time_end = time.time()
+	print('Training finished')
+	print('TIME: %9.3f sec    EPOCHS: %6d    SPEED: %9.3f sec/epoch' % (time_end - time_start, epoch, (time_end - time_start) / epoch))
+
 	if best_p is not None:
 		zipp(best_p, t_params)
 	else:
 		best_p = unzip(t_params)
 
+	print('Final predicting...')
 	use_dropout.set_value(0.)
-	batches_train = get_batches(len(samples_train), batch_size_train)
-	preds_train, err_train = pred_error(f_prob, samples_train, batches_train, mat_train)
+	preds_train, err_train = pred_error(f_prob, samples_train, get_batches(len(samples_train), batch_size_pred), mat_train)
 	preds_val, err_val = pred_error(f_prob, samples_val, batches_val, mat_val)
 	preds_test, err_test = pred_error(f_prob, samples_test, batches_test, mat_test)
-
-	print('Training: ', err_train, 'Validation: ', err_val, 'Testing: ', err_test)
+	print('ERR_TRA: %.6f    ERR_VAL: %.6f    ERR_TES: %.6f' % (err_train, err_val, err_test))
 	if path_save:
 		numpy.save(path_save, best_p)
 	numpy.save(path_out_train, preds_train)
 	numpy.save(path_out_val, preds_val)
 	numpy.save(path_out_test, preds_test)
-	print('The code run for %d epochs, with %f sec/epochs' % ((epoch + 1), (time_end - time_start) / (1. * (epoch + 1))))
-	print(('Training took %.1fs' % (time_end - time_start)))
-	return err_train, err_val, err_test
+	print('All works finished')
 
 
 if __name__ == '__main__':
 	main_model()
+	# For debugging, use the following args:
+	# main_model(max_samples_train=400, max_samples_val=50, max_samples_test=50, batch_size_train=50, batch_size_pred=50, max_epochs=10, freq_disp=1, freq_val=0, freq_save=0)
