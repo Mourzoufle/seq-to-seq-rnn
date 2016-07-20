4d3d856065d77a63600bef0324940e18102f9b55
Mourzoufle <Mourzoufle@163.com>
2016-07-20 16:12:17 +0800

    better progress bar

--- utils.py
+++ utils.py
@@ -52,9 +52,9 @@ class ProgressBar(object):
     def __init__(self, max_items, max_size, header='', char='#'):
         self.__max_items = max_items    # Total number of items to be processed
         self.__max_size = max_size      # Max size of the progress bar - Total number of characters and spaces
+        self.__size = -1                # Current size of the progress bar - Total number of characters
         self.__header = header          # Header of the progress bar
         self.__char = char              # Character indicating the current progress
-        self.__threshold = 1            # Threshold to trigger the progress bar to be redrawn
         self.__is_paused = True         # Flag indicating if the current progress is paused and has printed \n
         self.__is_finished = False      # Flag indicating if the current progress us already finished
 
@@ -66,11 +66,12 @@ class ProgressBar(object):
         if self.__is_finished:
             return
 
-        if not self.__is_paused and len(items) < self.__threshold:
+        size = len(items) * self.__max_size / self.__max_items
+        if not self.__is_paused and size <= self.__size:
             return
 
         self.__is_paused = False
-        size = len(items) * self.__max_size / self.__max_items
+        self.__size = size
         stdout.write('\r%s %3d%% |%s%s| %s: %f' % (self.__header, size * 100 / self.__max_size, self.__char * size, ' ' * (self.__max_size - size), tag, numpy.mean(items)))
 
         if len(items) == self.__max_items:
@@ -78,8 +79,6 @@ class ProgressBar(object):
             self.__is_finished = True
             stdout.write('\n')
             return
-
-        self.__threshold = min(int((self.__max_items - 1) * float(size + 1) / self.__max_size + 1), self.__max_items)
 
 
     def pause(self):
